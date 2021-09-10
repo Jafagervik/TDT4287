d9395a61497dc4824f610b9b5508a6fb56d724e6
Jafagervik <jorgenafagervik@gmail.com>
2021-09-10 16:48:59 +0200

    40% done with lecture 3

---
+++ heuristic_alignment.py
@@ -0,0 +1,45 @@
+"""
+
+Homologus Genes
+    - Often similar functions 
+    - Sequence similarities
+
+Sequence similarity indicates homology
+"""
+
+
+def init_matrix(m: int, n: int):
+    return [[false] * (n) for i in range(m)]
+
+
+def find_diagonals(matrix) -> list:
+    pass
+
+
+def homology_search(S: str, D: str) -> list[str]:
+    """
+    Dot plot matrix to figure it out
+    >> input: Gene sequence S and gene sequence database D
+    >> output: All genes g in D that are homologs of S
+    """
+    homologs = []
+
+    m = len(S) + 1
+    n = len(D) + 1
+    dot_plot_matrix = init_matrix(m, n)
+
+    for i in range(1, m):
+        for j in range(1, n):
+            if S[i - 1] == D[j - 1]:
+                eyeballing(dot_plot_matrix, i, j)
+    return dot_plot_matrix
+
+
+def eyeballing(matrix, i, j):
+    """This could result in lots of noice we're not interested in"""
+    matrix[i][j] = True
+
+
+def kmer(matrix, i, j, k):
+    """Now we need substrings to be at least of size k"""
+    raise NotImplementedError()
---
+++ hypothesis_testing.py
@@ -0,0 +1,26 @@
+from .local_alignment import la
+import random
+
+""" How to Randomize?
+
+Universal
+    - Letter distribution from database
+Global
+    - Letter distribution from similar sequences
+Local
+    - Letter distribution from Q and D
+    - Shuffle whole D
+    - Shuffle regions of D with similar sequence composition
+
+
+"""
+
+
+def randomization(Q: str, D: str, iterations=10000):
+    """Randomized-based hhypothesis testing. Check if any random string from database is bigger than D with LA algorithm."""
+    x = la(Q, D)
+    count = 0
+    for i in range(iterations):
+        if la(Q, random.rand(D)) >= x:
+            count += 1
+    return (count + 1) / (iterations + 1)
